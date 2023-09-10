a471212193f9609533d599cc4bc0eca6945a41c4
Shashikant yadav <127616023+shashikant270196@users.noreply.github.com>
2023-09-10 18:40:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const path = require('path');
+<script>
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-const app = express();
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim();
+}
 
-app.use(express.static(__dirname))
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
-});
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
+document.querySelector('#bands').innerHTML =
+  sortedBands
+    .map(band => `<li>${band}</li>`)
+    .join('');
 
+console.log(sortedBands);
+
+</script>
